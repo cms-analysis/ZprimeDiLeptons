09ed94f4759ff4fba51a326672adc00b2a00f9a9
Jared Sturdy <sturdy@cern.ch>
2017-10-12 18:20:12 +0200

    helpers for processing many samples with multicrab

---
+++ Analyzer/test/ci_2mu_samples.py
@@ -0,0 +1,150 @@
+cito2musamples = {
+    "Lam16": {
+        "Con": {
+            "LR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam16TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam16TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam16TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "LL": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam16TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M2000_CUETP8M1_Lam16TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam16TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam16TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "RR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam16TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam16TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam16TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            },
+        "Des": {
+            "LR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam16TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam16TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam16TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "LL": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam16TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam16TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam16TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "RR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam16TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam16TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam16TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            },
+        },
+    "Lam22": {
+        "Con": {
+            "LR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam22TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam22TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam22TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "LL": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam22TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M2000_CUETP8M1_Lam22TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam22TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam22TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "RR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam22TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam22TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam22TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            },
+        "Des": {
+            "LR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam22TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam22TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam22TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "LL": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam22TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam22TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam22TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "RR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam22TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam22TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam22TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            },
+        },
+    "Lam28": {
+        "Con": {
+            "LR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam28TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam28TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam28TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "LL": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam28TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M2000_CUETP8M1_Lam28TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam28TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam28TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "RR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam28TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam28TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam28TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            },
+        "Des": {
+            "LR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam28TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam28TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam28TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "LL": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam28TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam28TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam28TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "RR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam28TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam28TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam28TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            },
+        },
+    "Lam34": {
+        "Con": {
+            "LR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam34TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam34TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam34TeVConLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "LL": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam34TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M2000_CUETP8M1_Lam34TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam34TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam34TeVConLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "RR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam34TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam34TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam34TeVConRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            },
+        "Des": {
+            "LR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam34TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam34TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam34TeVDesLR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "LL": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam34TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam34TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam34TeVDesLL_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            "RR": [
+                ['/CITo2Mu_M1300_CUETP8M1_Lam34TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M300_CUETP8M1_Lam34TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ['/CITo2Mu_M800_CUETP8M1_Lam34TeVDesRR_13TeV_Pythia8_Corrected-v4/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', None],
+                ],
+            },
+        },
+    }
