fde77af7bbd91f71183d736c3c77f11942f53800
Jared Sturdy <sturdy@cern.ch>
2017-12-14 18:23:53 +0100

    update driver settings for DY samples

--- Analyzer/test/ci_driver.py
+++ Analyzer/test/ci_driver.py
@@ -1,27 +1,52 @@
 import os
 
 class ciDriver:
-    
+
     def __init__(self,*args,**kwargs):
         from ci_2mu_samples import cito2musamples
         from ci_2e_samples import cito2esamples
+        from dy_2mu_samples import dyto2musamples
+        from dy_2e_samples import dyto2esamples
 
         print("args:",args)
         print("kwargs",kwargs)
-        self.lam  = args[0]
-        self.intf = args[1]
-        self.heli = args[2]
+
+        self.isDY = False
+        self.dy   = args[0]
+        self.lam  = None
+        self.intf = None
+        self.heli = None
+
+        if self.dy in ["DYTo2L"]:
+            self.isDY = True
+        else:
+            self.isDY = False
+            self.lam  = args[0]
+            self.intf = args[1]
+            self.heli = args[2]
+            pass
+
         if self.lam in ["Lam10","Lam100k"]:
             self.dbs  = "phys03"
         else:
             self.dbs  = "global"
             pass
 
-        self.cito2musamples = cito2musamples[self.lam][self.intf][self.heli]
-        self.cito2esamples  = cito2esamples[self.lam][self.intf][self.heli]
+        if self.isDY:
+            self.dyto2musamples = dyto2musamples["DY"]
+            self.dyto2esamples  = dyto2esamples["DY"]
+        else:
+            self.cito2musamples = cito2musamples[self.lam][self.intf][self.heli]
+            self.cito2esamples  = cito2esamples[self.lam][self.intf][self.heli]
+            pass
         pass
 
     def getJobOptions(self):
+        if self.isDY:
+            mcfiles = list(mu for mu in self.dyto2musamples) + list(e for e in self.dyto2esamples)
+        else:
+            mcfiles = list(mu for mu in self.cito2musamples) + list(e for e in self.cito2esamples)
+            pass
 
         ci_job_options = {
             "storageSite":"T3_US_FNALLPC",
@@ -29,8 +54,7 @@ class ciDriver:
             "cfgFile":    "runMakeZprimeMiniAodTreeMC_cfg.py",
             "dbs":        self.dbs,
             "datasets": {
-                "MC": list(mu for mu in self.cito2musamples) + list(e for e in self.cito2esamples),
-                # "MC":   list(mu for mu in self.cito2musamples),
+                "MC": mcfiles,
                 "data": [
                     # ["/SingleMuon/Run2016B-23Sep2016-v3/MINIAOD",json],
                     # ["/SingleMuon/Run2016C-23Sep2016-v1/MINIAOD",json],
